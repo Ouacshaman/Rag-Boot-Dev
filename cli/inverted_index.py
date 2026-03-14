dbe06c5e2c620ce76e11f17f0027a7cfd98c8cc9
Ouacshaman <shaquanhong@gmail.com>
2026-03-14 17:10:59 -0400

    mapped out the structure to inverted index class

---
+++ cli/inverted_index.py
@@ -0,0 +1,16 @@
+class InvertedIndex:
+    def __init__(self, index, docmap):
+        self.index = index
+        self.docmap = docmap
+
+    def __add_documents(self, doc_id, text):
+        pass
+
+    def get_documents(self, term):
+        pass
+
+    def build():
+        pass
+
+    def save():
+        pass
