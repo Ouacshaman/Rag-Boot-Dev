b92a0474f0460f8dea5f1ba6085652e3ee166597
Ouacshaman <shaquanhong@gmail.com>
2026-04-13 02:24:10 -0400

    completed add document and get document

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,15 +1,25 @@
+from search_cmd import tokenization
+
+
 class InvertedIndex:
     def __init__(self, index, docmap):
         self.index = index
         self.docmap = docmap
 
     def __add_documents(self, doc_id, text):
-        pass
+        token = tokenization(text)
+        for v in token:
+            if v in self.index:
+                self.index = self.index[v].append(doc_id)
+            else:
+                self.index[v] = [doc_id]
 
     def get_documents(self, term):
-        pass
+        term = term.lower()
+        out = self.index[term].sort()
+        return out
 
-    def build():
+    def build(movies):
         pass
 
     def save():
