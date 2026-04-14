447eef9fd34f427486276c8f72b47d23e027c3a3
Ouacshaman <shaquanhong@gmail.com>
2026-04-14 01:00:43 -0400

    completed build

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -10,17 +10,21 @@ class InvertedIndex:
         token = tokenization(text)
         for v in token:
             if v in self.index:
-                self.index = self.index[v].append(doc_id)
+                self.index = self.index[v].set(doc_id)
             else:
-                self.index[v] = [doc_id]
+                self.index[v] = {doc_id}
 
     def get_documents(self, term):
         term = term.lower()
         out = self.index[term].sort()
         return out
 
-    def build(movies):
-        pass
+    def build(self, movies, doc_id):
+        for i in range(0, len(movies), 1):
+            joined = f"{movies[i]["title"]} {movies[i]["descriptions"]}"
+            self.__add_documents(doc_id, joined)
+            self.docmap[doc_id] = joined
+        return
 
     def save():
         pass
